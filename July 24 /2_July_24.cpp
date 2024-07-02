04912b7488f3b9b48093b947cf636189254ec969
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2024-07-02 15:42:31 +0530

    Update and rename 2_Feb_24.cpp to 2_July_24.cpp

---
+++ July 24 /2_July_24.cpp
@@ -0,0 +1,335 @@
+/*
+The structure of linked list is the following
+struct Node
+{
+    string data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+#include <bits/stdc++.h>
+#define nfs cin.tie(NULL)->sync_with_stdio(false)
+using namespace std;
+using ll = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using pi = pair<ll, ll>;
+using vi = vector<ll>;
+template <class T>
+using vd = vector<T>;
+template <class T>
+using vdd = vector<vd<T>>;
+template <class T>
+using vddd = vector<vdd<T>>;
+template <class T>
+using vdddd = vector<vddd<T>>;
+template <class T>
+using vddddd = vector<vdddd<T>>;
+template <class T>
+using q = queue<T>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...) vector<vector<vector<vector<type>>>> name( a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
+#define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
+#define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
+#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
+#define FOR1_R(a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR2_R(i, a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR3_R(i, a, b) for (ll i = (b)-1; i >= ll(a); --i)
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+#define all(x) x.begin(), x.end()
+#define len(x) ll(x.size())
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+const int mod = 1e9 + 7;
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define INT(...) int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; IN(__VA_ARGS__)
+#define CHR(...) char __VA_ARGS__; IN(__VA_ARGS__)
+#define DBL(...) long double __VA_ARGS__; IN(__VA_ARGS__)
+#define VEC(type, name, size) vector<type> name(size); read(name)
+#define VV(type, name, h, w) vector<vector<type>> name(h, vector<type>(w)); read(name)
+void read(int &a) { cin >> a; }
+void read(long long &a) { cin >> a; }
+void read(char &a) { cin >> a; }
+void read(double &a) { cin >> a; }
+void read(long double &a) { cin >> a; }
+void read(string &a) { cin >> a; }
+template <class T, class S> void read(pair<T, S> &p) { read(p.first), read(p.second); }
+template <class T> void read(vector<T> &a) {for(auto &i : a) read(i);}
+template <class T> void read(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {read(head);IN(tail...);}
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {os << A.fi << " " << A.se;return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+void printn() {cout << "\n";cout.flush();}
+void print() {cout << ' ';cout.flush();}
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+void YES(bool t = 1) { print(t ? "YES" : "NO"); }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { print(t ? "yes" : "no"); }
+void no(bool t = 1) { yes(!t); }
+class Solution {
+  public:
+    bool compute(Node* head) {
+        // Your code goes here
+        // nfs;
+        string ans="";
+        Node* head1=head;
+        while(head1){
+            ans+=(head1->data);
+            head1=head1->next;
+        }
+        int start=0;
+        int end=ans.size()-1;
+        while(start<end){
+            if(ans[start] != ans[end])return false;
+            start++;end--;
+        }
+        return true;
+    }
+};
+
+
+
+// User function Template for Java
+class Solution {
+    public boolean compute(Node root) {
+        String ans="";
+        while(root != null){
+            ans+=(root.data);
+            root=root.next;
+        }
+        int start=0;
+        int end=ans.length()-1;
+        while(start<end){
+            if(ans.charAt(start) != ans.charAt(end))return false;
+            start++;end--;
+        }
+        return true;
+    }
+}
+
+
+
+// User function Template for javascript
+
+/*LINKED LIST NODE
+class Node {
+  constructor(x){
+    this.data = x;
+    this.next = null;
+  }
+}
+*/
+
+/**
+ * @param {Node} head
+ * @return {boolean}
+ */
+
+class Solution {
+    compute(head) {
+        // code here
+        let ans="";
+        while(head != null){
+            ans+=(head.data);
+            head=head.next;
+        }
+        let start=0;
+        let end=ans.length-1;
+        while(start<end){
+            if(ans[start++] != ans[end--])return false;
+        }
+        return true;
+    }
+}
+
+
+#include <bits/stdc++.h>
+#define nfs cin.tie(NULL)->sync_with_stdio(false)
+using namespace std;
+using ll = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using pi = pair<ll, ll>;
+using vi = vector<ll>;
+template <class T>
+using vd = vector<T>;
+template <class T>
+using vdd = vector<vd<T>>;
+template <class T>
+using vddd = vector<vdd<T>>;
+template <class T>
+using vdddd = vector<vddd<T>>;
+template <class T>
+using vddddd = vector<vdddd<T>>;
+template <class T>
+using q = queue<T>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...) vector<vector<vector<vector<type>>>> name( a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
+#define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
+#define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
+#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
+#define FOR1_R(a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR2_R(i, a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR3_R(i, a, b) for (ll i = (b)-1; i >= ll(a); --i)
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+#define all(x) x.begin(), x.end()
+#define len(x) ll(x.size())
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+const int mod = 1e9 + 7;
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define INT(...) int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; IN(__VA_ARGS__)
+#define CHR(...) char __VA_ARGS__; IN(__VA_ARGS__)
+#define DBL(...) long double __VA_ARGS__; IN(__VA_ARGS__)
+#define VEC(type, name, size) vector<type> name(size); read(name)
+#define VV(type, name, h, w) vector<vector<type>> name(h, vector<type>(w)); read(name)
+void read(int &a) { cin >> a; }
+void read(long long &a) { cin >> a; }
+void read(char &a) { cin >> a; }
+void read(double &a) { cin >> a; }
+void read(long double &a) { cin >> a; }
+void read(string &a) { cin >> a; }
+template <class T, class S> void read(pair<T, S> &p) { read(p.first), read(p.second); }
+template <class T> void read(vector<T> &a) {for(auto &i : a) read(i);}
+template <class T> void read(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {read(head);IN(tail...);}
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {os << A.fi << " " << A.se;return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+void printn() {cout << "\n";cout.flush();}
+void print() {cout << ' ';cout.flush();}
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+void YES(bool t = 1) { print(t ? "YES" : "NO"); }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { print(t ? "yes" : "no"); }
+void no(bool t = 1) { yes(!t); }
+class Solution {
+public:
+    vector<int> intersect(vector<int>& n1, vector<int>& n2) {
+       nfs;
+       vector<int>ans;
+       int i=0;
+       int j=0;
+       sort(all(n1));
+       sort(all(n2));
+       while(i<n1.size() && j<n2.size()){
+            if(n1[i]==n2[j]){
+                ans.pb(n1[i]);
+                i++;
+                j++;
+            }
+            else if(n1[i]<n2[j])i++;
+            else j++;
+       }
+       return ans;
+    }
+};
+
+class Solution {
+    public int[] intersect(int[] n1, int[] n2) {
+        int i=0;
+        int j=0;
+        int k=0;
+        Arrays.sort(n1);
+        Arrays.sort(n2);
+        while(i<n1.length && j<n2.length){
+            if(n1[i] == n2[j]){
+                n1[k++]=n1[i];
+                i++;
+                j++;
+            }
+            else if(n1[i]<n2[j])i++;
+            else j++;
+        }
+        return Arrays.copyOfRange(n1,0,k);
+    }
+}
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(n1, n2) {
+    var ans=[];
+    let i=0;
+    let j=0;
+    n1.sort((a, b) => a - b);
+    n2.sort((a, b) => a - b);
+    while(i<n1.length && j<n2.length){
+        if(n1[i]==n2[j]){
+            ans.push(n1[i]);
+            i++;
+            j++;
+        }
+        else if(n1[i]<n2[j])i++;
+        else j++;
+    }
+    return ans;
+};
