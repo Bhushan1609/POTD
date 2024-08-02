5629f5422c51706cdccd3fb99fcc8ac6add8bbda
Bhushan Thombare <crackthecode1609@gmail.com>
2024-08-02 16:53:17 +0530

    02_August_2024.cpp

---
+++ August/02_August_2024.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+#define nfs cin.tie(NULL)->sync_with_stdio(false)
+using namespace std;
+using ll = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using pi = pair<ll, ll>;
+using vi = vector<ll>;
+template <class T>
+using vd = vector<T>;
+template <class T>
+using vdd = vector<vd<T>>;
+template <class T>
+using vddd = vector<vdd<T>>;
+template <class T>
+using vdddd = vector<vddd<T>>;
+template <class T>
+using vddddd = vector<vdddd<T>>;
+template <class T>
+using q = queue<T>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...) vector<vector<vector<vector<type>>>> name( a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
+#define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
+#define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
+#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
+#define FOR1_R(a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR2_R(i, a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR3_R(i, a, b) for (ll i = (b)-1; i >= ll(a); --i)
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+#define all(x) x.begin(), x.end()
+#define len(x) ll(x.size())
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+const int mod = 1e9 + 7;
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define INT(...) int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; IN(__VA_ARGS__)
+#define CHR(...) char __VA_ARGS__; IN(__VA_ARGS__)
+#define DBL(...) long double __VA_ARGS__; IN(__VA_ARGS__)
+#define VEC(type, name, size) vector<type> name(size); read(name)
+#define VV(type, name, h, w) vector<vector<type>> name(h, vector<type>(w)); read(name)
+void read(int &a) { cin >> a; }
+void read(long long &a) { cin >> a; }
+void read(char &a) { cin >> a; }
+void read(double &a) { cin >> a; }
+void read(long double &a) { cin >> a; }
+void read(string &a) { cin >> a; }
+template <class T, class S> void read(pair<T, S> &p) { read(p.first), read(p.second); }
+template <class T> void read(vector<T> &a) {for(auto &i : a) read(i);}
+template <class T> void read(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {read(head);IN(tail...);}
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {os << A.fi << " " << A.se;return os;}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+void printn() {cout << "\n";cout.flush();}
+void print() {cout << ' ';cout.flush();}
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+void YES(bool t = 1) { print(t ? "YES" : "NO"); }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { print(t ? "yes" : "no"); }
+void no(bool t = 1) { yes(!t); }
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        nfs;
+        int sum=accumulate(all(nums) , 0),ans=1e9;
+        int sumOnes=0;
+        nums.insert(nums.end() , all(nums));
+        FOR(i,len(nums)){
+            if(i<sum){
+                sumOnes += nums[i];
+            }
+            else{
+                ans=min(ans,sum - sumOnes);
+                sumOnes += (nums[i] - nums[i-sum]);
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+class Solution {
+    public int minSwaps(int[] nums) {
+        int ans=Integer.MAX_VALUE;
+        int sum=0,sumOnes=0;
+        int[] nums1=new int[2 * nums.length];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            nums1[i]=nums[i];
+        }
+        for(int i=nums.length;i<nums1.length;i++){
+            nums1[i]=nums[i-nums.length];
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(i<sum){
+                sumOnes+=nums1[i];
+            }
+            else{
+                ans=Math.min(ans , sum-sumOnes);
+                sumOnes += (nums1[i] - nums1[i-sum]);
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSwaps = function(nums) {
+    var ans=Infinity;
+    let sum=0,sumOnes=0;
+    let nums1=[];
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        nums1.push(nums[i]);
+    }
+    for(let i=nums.length;i<2*nums.length;i++){
+        nums1.push(nums[i-nums.length]);
+    }
+    for(let i=0;i<nums1.length;i++){
+        if(i<sum){
+            sumOnes+=nums1[i];
+        }
+        else{
+            ans=parseInt(Math.min(ans , sum-sumOnes));
+            sumOnes += (nums1[i] - nums1[i-sum]);
+        }
+    }
+    return ans;
+};
