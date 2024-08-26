4207b6504949dd275482dc4d7f79094a29e088b3
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2024-08-26 13:48:53 +0530

    26_August_2024.cpp

---
+++ August/26_August_2024.cpp
@@ -0,0 +1,233 @@
+#include<bits/stdc++.h>
+#define SPEEDUP cin.tie(NULL)->sync_with_stdio(false)
+
+using namespace std;
+using ll=long long;
+using u32=unsigned int;
+using u64=unsigned long long;
+
+template <class T,class P>
+using pi=pair<T,P>;
+
+#define INT(...)int __VA_ARGS__; IN(__VA_ARGS__)
+#define LL(...)ll __VA_ARGS__; IN(__VA_ARGS__)
+#define STR(...)string __VA_ARGS__; IN(__VA_ARGS__)
+#define CHR(...)char __VA_ARGS__; IN(__VA_ARGS__)
+#define DBL(...)long double __VA_ARGS__; IN(__VA_ARGS__)
+
+void read(int &a){cin>>a;}
+void read(long long &a){cin>>a;}
+void read(char &a){cin>>a;}
+void read(double &a){cin>>a;}
+void read(long double &a){cin>>a;}
+void read(string &a){cin>>a;}
+
+template<class T,class S>void read(pair<T,S>&p){read(p.first),read(p.second);}
+template<class T>void read(vector<T>&a){for(auto &i:a)read(i);}
+template <class T>void read(T &a){cin >> a;}
+void IN(){}
+template<class Head,class... Tail>void IN(Head &head, Tail &...tail) {read(head);IN(tail...);}
+template<typename T,typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A){os<<A.fi<<" "<<A.se;return os;}
+template <typename T>
+ostream& operator<<(ostream& os,const vector<T>&A) {
+    for(size_t i=0;i<A.size();i++){
+        if(i) os<<" ";
+        os << A[i];
+    }
+    return os;
+}
+void print(){cout << '\n';cout.flush();}
+template <class Head,class... Tail>
+void print(Head&& head, Tail&&... tail) {
+    cout<<head;
+    if(sizeof...(Tail)) cout<<" ";
+    print(forward<Tail>(tail)...);
+}
+
+#define dbg(x...){cout<< "("<< #x<<")"<< " = (";Print(x);} 
+template<typename T1>void Print(T1 t1) {cout<< t1 << " )" <<endl;} 
+template<typename T1,typename... T2>
+void Print(T1 t1, T2... t2) {cout<<t1<<" , ";Print(t2...);}
+
+void YES(bool t=1) {print(t?"YES":"NO");}
+void NO(bool t=1) {YES(!t);}
+void Yes(bool t=1) {print(t?"Yes":"No");}
+void No(bool t=1) {Yes(!t);}
+void yes(bool t=1) {print(t?"yes":"no");}
+void no(bool t=1) {yes(!t);}
+
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+#define all(x) x.begin(), x.end()
+#define len(x) ll(x.size())
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define SUM(v) accumulate(all(v),(int)0)
+
+template <class T>
+using vd=vector<T>;
+template <class T>
+using vdd=vector<vd<T>>;
+template <class T>
+using vddd=vector<vdd<T>>;
+template <class T>
+using vdddd=vector<vddd<T>>;
+template <class T>
+using vddddd=vector<vdddd<T>>;
+template <class T>
+using q=queue<T>;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-const-variable"
+const int mod = 1e9 + 7; 
+vd<pi<int, int>> gridMoves = { {-1, 0}, {0, -1}, {0, 1}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
+#pragma GCC diagnostic pop
+
+/*MaxHeap*/
+template <class T>
+using pq=priority_queue<T>;
+/*MinHeap*/
+template <class T>
+using pqg=priority_queue<T,vector<T>, greater<T>>;
+
+/*FOR*/
+#define FOR1(a) for(ll _=0;_<ll(a);++_)
+#define FOR2(i,a) for(ll i=0;i<ll(a);++i)
+#define FOR3(i,a,b) for(ll i=a;i<ll(b);++i)
+#define FOR4(i,a,b,c) for (ll i =a;i<ll(b);i+=(c))
+/*FOR_R*/
+#define FOR1_R(a) for (ll i =(a)-1;i>=ll(0);--i)
+#define FOR2_R(i,a) for (ll i=(a)-1;i>=ll(0);--i)
+#define FOR3_R(i,a,b) for(ll i=(b)-1;i>=ll(a);--i)
+
+/*Declare 2d/3d(type,name,size,size1,size2,....)*/
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__))))
+#define vvvv(type, name, a, b, c, ...) vector<vector<vector<vector<type>>>> name( a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+
+/*Read 1d/2d(type,name,size,size1)*/
+#define VEC(type, name, size) vector<type> name(size); read(name)
+#define VV(type, name, h, w) vector<vector<type>> name(h, vector<type>(w)); read(name)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+void dfs(Node* root,Node* parent,vd<int>&answer){
+	for(auto i:root->children){
+		if(i!=parent) dfs(i,root,answer);
+	}
+    answer.eb(root->val);
+}
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        SPEEDUP;
+        if(!root)return {};
+        vd<int>answer;
+        dfs(root,NULL,answer);
+        return answer;
+    }
+};
+
+#ifdef LOCAL
+void solve() {
+    Solution Obj;
+    vd<string>input={};
+    return print(Obj.function(input));
+}
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+    return 0;
+}
+#endif
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public static void dfs(Node root,Node parent,List<Integer>answer){
+        if(root==null)return ;
+        for(Node i :root.children){
+            if(i != parent) dfs(i , root, answer);
+        }
+        answer.add(root.val);
+    }
+    public List<Integer> postorder(Node root) {
+        List<Integer>answer=new ArrayList<>();
+        if(root==null)return answer;
+        dfs(root,null,answer);
+        return answer;
+    }
+}
+
+
+/**
+ * // Definition for a _Node.
+ * function _Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {_Node|null} root
+ * @return {number[]}
+ */
+function dfs(root,parent,answer){
+    if(root==null)return ;
+    for(let i of root.children){
+        if(i != parent) dfs(i , root, answer);
+    }
+    answer.push(root.val);
+}
+var postorder = function(root) {
+    let answer=[];
+    if(root==null)return answer;
+    dfs(root,null,answer);
+    return answer;
+};
